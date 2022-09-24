915477febebd8cdad68ac84dc7c6e0003e21e9da
liluhao <2415306912@qq.com>
2022-09-24 16:31:19 +0800

    补充

---
+++ student.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.25 : Database - student
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`student` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `student`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `name` varchar(171) DEFAULT NULL,
+  `password` varchar(171) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`name`,`password`) values ('qeqrr','wqfewgtweg'),('qeqrr','wqfewgtweg'),(' ',''),(' 7777','7777');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `name` varchar(171) DEFAULT NULL,
+  `password` varchar(171) DEFAULT NULL,
+  `sex` varchar(171) DEFAULT NULL,
+  KEY `stu_sex` (`sex`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `student` */
+
+insert  into `student`(`name`,`password`,`sex`) values ('1','wqfewgtweg','1'),('3','7777','5'),('7','wqfewgtweg','7'),('11','asdaf','10008'),('20','asfsaf','555');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
